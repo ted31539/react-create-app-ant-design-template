fdbd0fac82165e1e7487e62b3da81bf970adf8ef
Ted <ted31539@gmail.com>
2022-10-12 16:53:12 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+import './less/app.less';
+import './scss/all.scss'
+
+function App() {
+  return (
+    <div className='App'>
+      <Button type='primary'>Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
